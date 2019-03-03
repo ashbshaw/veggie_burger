24f0daeb9b7fbcd22ab58edcc5846806860c2764
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-03-02 21:10:15 -0600

    first push

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("./connection.js");
+
+//How to translate
+// var orm = {
+//     selectWhere: function(tableInput, colToSearch, valOfCol) {
+//       var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+//       connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+//         if (err) throw err;
+//         console.log(result);
+//       });
+//     },
+
+selectAll()
+insertOne()
+updateOne()
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Cauliflower');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean');
+INSERT INTO burgers (burger_name) VALUES ('The Garbo');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Shiitake');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger Classic');
