5fff8ba0ba7fa5f1c6944ecfa2cf6f62b0a0c12f
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-03-09 20:07:56 -0600

    additional styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,46 +18,32 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    //     burger.create([
-    //         "burger_name", "devoured"
-    //     ], [
-    //             req.body.burger_name, req.body.devoured
-    //         ], function (result) {
-    //             // Send back the ID of the new quote
-    //             res.json({ id: result.insertId });
-    //         });
-    // });
-
-    burger.create([req.body.burger_name], function (result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
-    });
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            // Send back the ID of the new burger
+            res.json({ id: result.insertId });
+        });
 });
+
 
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
-    // console.log("condition", condition);
-
-    // burger.update({
-    //     devoured: req.body.devoured
-    // }, condition, function (result) {
-    //     if (result.changedRows == 0) {
-    //         // If no rows were changed, then the ID must not exist, so 404
-    //         return res.status(404).end();
-    //     } else {
-    //         res.status(200).end();
-    //     }
-    // });
-
-    burger.update(req.body, condition, function(result){
-        if (result.changedRows === 0) {
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
-            } else {
-                res.status(200).end();
-            };
-        });
+        } else {
+            res.status(200).end();
+        }
     });
+
+});
 
 router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
